a08f28f7d4b2bc3a1311b57e6d93451c21455fd1
Shiven <shiven.assetmonk@gmail.com>
2021-07-14 15:10:59 +0530

    display section

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
           minlength: 3
         },
         phone: {
-          required: true,
-          phone: true
+          required: true
+          
         },
         email: {
           required: true,
@@ -15,18 +15,77 @@ $(document).ready(function(){
         },
         description: {
           required: true,
-          minlength: 15
+          minlength: 2
         },
         appointment: {
           required: true
         }
-    }
+    },
+    messages : {
+        name: {
+          required: "Please enter the name",
+          minlength: "Name must be atleast 3 letters"
+        },
+        phone: {
+          required: "Please enter phone number in correct format"
+          
+        },
+        email: {
+          required: "Please enter the email address",
+          email: "The email should be in the format: abc@domain.tld"
+        },
+        description: {
+          required: "Please describe the problem",
+          minlength: "The description is too small"
+        },
+        appointment: {
+            required: "Please select a slot for the appointment"
+          }
+      }
       
       
     })
     
-    $('#submit').on('click', function() {
+    $('#submit').on('click', function(e) {
+      e.preventDefault;
+
+        let name=e.currentTarget.form[0].value;
+        let pn= e.currentTarget.form[1].value;
+        let email= e.currentTarget.form[2].value;
+        let desc= e.currentTarget.form[3].value;
+        let appoint= e.currentTarget.form[4].value;
+        let gender='';
+        if(e.currentTarget.form[5].checked==true){
+          gender=e.currentTarget.form[5].value;
+        }
+        else if(e.currentTarget.form[6].checked==true){
+          gender=e.currentTarget.form[6].value;
+        }
+        else{
+          gender=e.currentTarget.form[7].value;
+        }
+        
+        //console.log(gender);
+        
+
         console.log($("#formid").valid());
+        if($("#formid").valid()){
+            //  $("#formid").hide();
+            $("#formid :input").prop("disabled", true);
+            $("#display-section").prop("hidden", false);
+
+            
+            $("#nameDisplay").text("Name : "+name);
+            $("#numDisplay").text("Phone Number : "+pn);
+            $("#emailDisplay").text("Email : "+email);
+            $("#descDisplay").text("Problem Description : \n"+desc);
+            $("#dateDisplay").text("Appointment Slot : "+appoint);
+            $("#genderDisplay").text("Appointment Slot : "+gender);
+
+            
+
+            
+        };
         console.log("It is submitted")
 
     });
